a62d4dd580faa20016042fa52928559cccc8d1f3
orlandovillasenor <orlandovillasenor@me.com>
2014-02-07 20:51:08 -0600

    exercise High/Low game

--- php/highlow.php
+++ php/highlow.php
@@ -1,35 +1,64 @@
 <?php
 
-$rand_number = mt_rand(1, 100);
+$min = $argv[1];
+
+$max = $argv[2];
+
+if ($argc != 3) {
+	echo "Command Line Error";
+	exit(0);
+}
+
+if (!is_numeric($argv[1])) {
+	echo "Command Line Error";
+	exit(0);
+}
+
+if (!is_numeric($argv[2])) {
+	echo "Command Line Error";
+	exit(0);
+}
+
+if ($argv[1] >= $argv[2]) {
+	echo "Command Line Error";
+	exit(0);
+}
+
+$rand_number = mt_rand($min, $max);
 
 fwrite(STDOUT, "Guess a number between 1 and 100: ");
 
 $user_number = fgets(STDIN);
 
+$user_number = trim($user_number);
+
 $guess_count = 1;
 
-while ($user_number != $rand_number) {
-	
-	if ($user_number < $rand_number) {
-		echo "GO HIGHER! ";
-	}		
-	if ($user_number > $rand_number) {
-		echo "GO LOWER! ";
-	}
-	$user_number = fgets(STDIN);
-	$guess_count++;
+if (is_numeric($user_number)) {
+
+	while ($user_number != $rand_number) {
+		if ($user_number < $rand_number) {
+			echo "GO HIGHER! ";
+		}		
+		elseif ($user_number > $rand_number) {
+			echo "GO LOWER! ";
+		} 
+		$user_number = fgets(STDIN);
+		$guess_count++;
+		}
+} else {
+	echo "YOU LOSE! ";
+	exit(0);
 }
+
 
 if ($guess_count == 1) {
-		echo "Amazing! You got it on the first try. You should play the lottery tonight.\n";
-	} elseif ($guess_count > 1 && $guess_count <= 5) {
-		echo "Good Guess! It only took you $guess_count guesses. That's not too shabby.\n";
-	} else {
-		echo "GOOD GUESS! It took you $guess_count guesses. You need to work on your psychic abilities though.\n";
+	echo "AMAZING! You got it on the first try. You should play the lottery tonight.\n";
+} elseif ($guess_count > 1 && $guess_count <= 5) {
+	echo "GOOD GUESS! It only took you $guess_count guesses. That's not too shabby.\n";
+} else {
+	echo "GOOD GUESS! It took you $guess_count guesses. You need to work on your psychic abilities though.\n";
 }
-
-
-
 
 exit(0);
 
