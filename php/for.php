58506adcf23d7ecbfee7a14d4aa9e7ff63a73ba6
orlandovillasenor <orlandovillasenor@me.com>
2014-02-08 14:28:24 -0600

    For loops exercise 2
    Refactor to allow user to choose increment. (count by 1, 2, 10, ...)

--- php/for.php
+++ php/for.php
@@ -8,19 +8,23 @@ $start_number = fgets(STDIN);
 
 $start_number = trim($start_number);
 
-fwrite(STDOUT, "Thanks!  Now enter a number that you want to count to: ");
+fwrite(STDOUT, "Now enter a number that you want to count to: ");
 
 $end_number = fgets(STDIN);
 
 $end_number = trim($end_number);
 
+fwrite(STDOUT, "Now choose increment (count by 1,2,5, etc.): ");
+
+$increment = fgets(STDIN);
+
+$increment = trim($increment);
+
 //display all numbers from start to finish using for loop
 
-for ($i = $start_number ; $i <= $end_number ; $i++) { 
+for ($i = $start_number ; $i <= $end_number ; $i += $increment) { 
 	echo "$i\n";
 
 }
-
-
 
 ?>
