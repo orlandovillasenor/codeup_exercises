4cb64a5dc906c40fde6c3de0f1f1a59f9919e03d
Orlando Villaseñor <orlandovillasenor@me.com>
2014-02-10 15:43:06 -0600

    Switch Exercise 1

---
+++ php/switch.php
@@ -0,0 +1,37 @@
+<?php
+
+// Set the default timezone
+date_default_timezone_set('America/Chicago');
+
+// Get Day of Week as number
+// 1 (for Monday) through 7 (for Sunday)
+$day_of_week = date('N');
+
+switch($day_of_week) {
+    case 1: 			
+		echo "Monday\n";
+		break;	
+    case 2: 
+        echo "Tuesday\n";
+    	break;
+    case 3: 
+        echo "Wednesday\n";
+    	break;    	
+    case 4:
+        echo "Thursday\n";
+        break;
+    case 5: 
+        echo "Friday\n";
+        break;
+    case 6: 
+        echo "Saturday\n";
+        break;
+    case 7: 
+        echo "Sunday\n";
+        break;    
+}
+
+
+
+
+?>
