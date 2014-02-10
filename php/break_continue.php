be931ad3dd7fa4b377a99c7d054b490d2a41a53c
Orlando Villaseñor <orlandovillasenor@me.com>
2014-02-10 14:59:06 -0600

    Break & Continue Exercise 2

--- php/break_continue.php
+++ php/break_continue.php
@@ -1,18 +1,18 @@
 <?php
 
-for ($i=0; $i <= 100; $i++) { 
-	if ($i % 2 == 0) {
-		echo "$i\n";
-		continue;
-	} 	
-}
+// for ($i=0; $i <= 100; $i++) { 
+// 	if ($i % 2 == 0) {
+// 		echo "$i\n";
+// 		continue;
+// 	} 	
+// }
 
-// for ($i=1; $i <= 100; $i++) { 
-// 	echo "$i\n";
-// 	if ($i > 10) {
-// 		break;
-// 	}
-//}
+for ($i=1; $i <= 100; $i++) { 
+	echo "$i\n";
+	if ($i >= 10) {
+		break;
+	}
+}
 
 
 
