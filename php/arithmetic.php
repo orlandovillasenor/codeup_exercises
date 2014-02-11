cc9ce43bc0579345538d0186976ca3e923cd56fc
Orlando Villaseñor <orlandovillasenor@me.com>
2014-02-11 14:55:34 -0600

    Functions 1 page 3 Exercise 1
    Refactor each function to return the result, removing the echo.

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -1,67 +1,64 @@
 <?php
 
-function error(){
-	echo "Error, arg 1 and arg 2 must both be numbers\n";
+function params_are_valid($a, $b){
+	if (!is_numeric($a) || !is_numeric($b)) {
+		echo "Error, arg \$a and arg \$b must both be numbers\n";
+		var_dump($a);
+		var_dump($b);
+	} else {
+		return TRUE;
+	}	
 }
 	
 function add($a, $b) {
-	if (is_numeric($a) && is_numeric($b)) {
-		echo $a + $b . "\n";
-	} else {
-		echo error();
-		//echo "Error, arg '{$a}' and arg '{$b}' must both be numbers\n";
-	}
+	params_are_valid($a, $b);
+	//if (is_numeric($a) && is_numeric($b)) {
+	return $a + $b . "\n";
 }
 
-add(10, 20);
+echo add(10, 20);
 
 function subtract($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) {
-		echo $a - $b . "\n";
-	} else {
-		echo error();
-		//echo "Error, arg '{$a}' and arg '{$b}' must both be numbers\n";
-	}
+	params_are_valid($a, $b);
+	//if (is_numeric($a) && is_numeric($b)) {
+	return $a - $b . "\n";
 }
 
-subtract(8, 4);
+echo subtract(8, 4);
 
 function multiply($a, $b) {
-    if (is_numeric($a) && is_numeric($b)) {
-		echo $a * $b . "\n";
-	} else {
-		echo error();
-		//echo "Error, arg '{$a}' and arg '{$b}' must both be numbers\n";
-	}
+	params_are_valid($a, $b);
+	//if (is_numeric($a) && is_numeric($b)) {
+	return $a * $b . "\n";
 }
 
-multiply(5, 4);
+echo multiply(5, 4);
 
 function divide($a, $b) {
-    if ($b == 0) {
+	if ($b == 0) {
     	echo "Error, arg \$b can not equal 0 because a number can not be divided by 0\n";
-    }
-
-    elseif (is_numeric($a) && is_numeric($b)) {
-		echo $a / $b . "\n";
-	} else {
-		echo error();
-		//echo "Error, arg '{$a}' and arg '{$b}' must both be numbers\n";
-	}
+    } elseif (params_are_valid($a, $b)) {
+	//if (is_numeric($a) && is_numeric($b)) {
+		return $a * $b . "\n";
+	}	
 }
 
-divide(20, 5);
+echo divide(20, 5);
 
-function modulus($a, $b){
-	if (is_numeric($a) && is_numeric($b)) {
-		echo $a % $b . "\n";
-	} else {
-		echo error();
-		//echo "Error, arg '{$a}' and arg '{$b}' must both be numbers\n";
-	}
+function modulus($a, $b) {
+	params_are_valid($a, $b);
+	//if (is_numeric($a) && is_numeric($b)) {
+	return $a % $b . "\n";
 }
 
-modulus(5, 4);
+echo modulus(5, 4);
 
+
+
+
+
+// if ($b == 0) {
+//     	echo "Error, arg \$b can not equal 0 because a number can not be divided by 0\n";
+//     }
 
 ?>
