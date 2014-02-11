c7b4793d63ea4fa78fcbe832eab18c1d4012e1cc
Orlando Villaseñor <orlandovillasenor@me.com>
2014-02-11 11:05:36 -0600

    Function 1 Exercise 2
    Add code after functions that calls each function with real numbers.

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -1,19 +1,29 @@
 <?php
 
 function add($a, $b) {
-    echo $a + $b;
+    echo $a + $b . "\n";
 }
+
+add(10, 2);
 
 function subtract($a, $b) {
-    echo $a - $b;
+    echo $a - $b . "\n";
 }
+
+subtract(8, 4);
 
 function multiply($a, $b) {
-    echo $a * $b;
+    echo $a * $b . "\n";
 }
 
+multiply(5, 8);
+
 function divide($a, $b) {
-    echo $a / $b;
+    echo $a / $b . "\n";
 }
+
+divide(20, 10);
+
+
 
 ?>
