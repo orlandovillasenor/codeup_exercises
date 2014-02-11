4bfac467219bdadb7b9ca09b2fa07662d1b0dcac
Orlando Villaseñor <orlandovillasenor@me.com>
2014-02-11 12:56:25 -0600

    Functions 2 page 2 Exercise 1
    Validate all the arguments, and display an error if the input is not
    numeric.

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -1,31 +1,51 @@
 <?php
 
 function add($a, $b) {
-    echo $a + $b . "\n";
+	if (is_numeric($a) && is_numeric($b)) {
+		echo $a + $b . "\n";
+	} else {
+		echo "Error, all inputs must be numbera\n";
+	}
 }
 
 add(10, 2);
 
 function subtract($a, $b) {
-    echo $a - $b . "\n";
+    if (is_numeric($a) && is_numeric($b)) {
+		echo $a - $b . "\n";
+	} else {
+		echo "Error, all inputs must be numbera\n";
+	}
 }
 
 subtract(8, 4);
 
 function multiply($a, $b) {
-    echo $a * $b . "\n";
+    if (is_numeric($a) && is_numeric($b)) {
+		echo $a * $b . "\n";
+	} else {
+		echo "Error, all inputs must be numbera\n";
+	}
 }
 
 multiply(5, 8);
 
 function divide($a, $b) {
-    echo $a / $b . "\n";
+    if (is_numeric($a) && is_numeric($b)) {
+		echo $a / $b . "\n";
+	} else {
+		echo "Error, all inputs must be numbera\n";
+	}
 }
 
 divide(20, 10);
 
 function modulus($a, $b){
-	echo $a % $b . "\n";
+	if (is_numeric($a) && is_numeric($b)) {
+		echo $a % $b . "\n";
+	} else {
+		echo "Error, all inputs must be numbera\n";
+	}
 }
 
 modulus(5, 4);
