fe595bfbf7f65c1155496e4233984d54cc6e399a
Orlando Villaseñor <orlandovillasenor@me.com>
2014-02-11 16:20:33 -0600

    Functions1 page 3 Exercise 2
    Validate divide by 0 errors, return FALSE if divide by 0 is attempted.

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -5,60 +5,62 @@ function params_are_valid($a, $b){
 		echo "Error, arg \$a and arg \$b must both be numbers\n";
 		var_dump($a);
 		var_dump($b);
+		return FALSE;
 	} else {
 		return TRUE;
 	}	
 }
 	
 function add($a, $b) {
-	params_are_valid($a, $b);
-	//if (is_numeric($a) && is_numeric($b)) {
-	return $a + $b . "\n";
+	if (params_are_valid($a, $b)){
+		return $a + $b . "\n";
+	} else {
+		return 0;
+	}
 }
 
 echo add(10, 20);
 
 function subtract($a, $b) {
-	params_are_valid($a, $b);
-	//if (is_numeric($a) && is_numeric($b)) {
-	return $a - $b . "\n";
+	if (params_are_valid($a, $b)){
+		return $a - $b . "\n";
+	} else {
+		return 0;
+	}
 }
 
 echo subtract(8, 4);
 
 function multiply($a, $b) {
-	params_are_valid($a, $b);
-	//if (is_numeric($a) && is_numeric($b)) {
-	return $a * $b . "\n";
+	if (params_are_valid($a, $b)){
+		return $a * $b . "\n";
+	} else {
+		return 0;
+	}
 }
 
 echo multiply(5, 4);
 
 function divide($a, $b) {
-	if ($b == 0) {
-    	echo "Error, arg \$b can not equal 0 because a number can not be divided by 0\n";
-    } elseif (params_are_valid($a, $b)) {
-	//if (is_numeric($a) && is_numeric($b)) {
+	if (params_are_valid($a, $b)) {
 		return $a / $b . "\n";
-	}	
+	} if ($b == 0) {
+		echo "Error, arg \$b can not equal 0 because a number can not be divided by 0\n";
+		return FALSE;
+	}
 }
 
 echo divide(20, 10);
 
 function modulus($a, $b) {
-	params_are_valid($a, $b);
-	//if (is_numeric($a) && is_numeric($b)) {
-	return $a % $b . "\n";
+	if (params_are_valid($a, $b)){
+		return $a % $b . "\n";
+	} else {
+		return 0;
+	}
 }
 
 echo modulus(5, 4);
 
-
-
-
-
-// if ($b == 0) {
-//     	echo "Error, arg \$b can not equal 0 because a number can not be divided by 0\n";
-//     }
 
 ?>
