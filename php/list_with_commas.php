dffc1d6a9e91725e3c60b1bd9e24ff25b8dd6f80
Orlando Villaseñor <orlandovillasenor@me.com>
2014-02-14 09:42:37 -0600

    Exploding and Imploding
    Turn your solution into a function named humanized_list(). You should
    be able to pass the $physicists_array as the only argument, and your
    function will return the result.
    Update your code to list the physicists by first name, in alphabetical
    order.
    
    Create a second argument to make alphabetical sorting optional.
    
    Default alphabetical sorting to false. If no args are passed, no
    sorting takes place.

--- php/list_with_commas.php
+++ php/list_with_commas.php
@@ -1,15 +1,41 @@
 <?php
 
-$physicists_array = ['Gordon Freeman', 'Samantha Carter', 'Sheldon Cooper', 'Quinn Mallory', 'Bruce Banner', 'Tony Stark'];
+//$physicists_array = ['Gordon Freeman', 'Samantha Carter', 'Sheldon Cooper', 'Quinn Mallory', 'Bruce Banner', 'Tony Stark'];
 
-//Create the $famous_fake_physicists string that lists the physicists and contains the "and" at the end.
+$physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+$physicists_array = explode(', ', $physicists_string);
 
-array_pop($physicists_array);
-array_push($physicists_array, 'and Tony Stark');
 
-$famous_fake_physicists = implode(', ', $physicists_array);
 
-echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.";
+// //Create the $famous_fake_physicists string that lists the physicists and contains the "and" at the end.
+
+// array_pop($physicists_array);
+// array_push($physicists_array, 'and Tony Stark');
+
+// $famous_fake_physicists = implode(', ', $physicists_array);
+
+// echo "Some of the most famous fictional theoretical physicists are {$famous_fake_physicists}.";
+
+//Turn your solution into a function named humanized_list(). You should be able to pass the $physicists_array as the only argument, and your function will return the result.
+
+function humanized_list($array, $sort  = FALSE){
+	
+	if ($sort == TRUE) {
+		sort($array);
+	}
+	//sort($array);
+	
+	$last = array_pop($array);
+	array_push($array, "and $last");
+	$string = implode(', ', $array);
+	echo $string;
+}
+
+
+
+humanized_list($physicists_array, TRUE);
+
+
 
 
 
