1c1575f464b5584a452a2a4676f27138ccb86845
Orlando Villaseñor <orlandovillasenor@me.com>
2014-02-14 01:05:04 -0600

    Searching in Arrays Exercise 3
    Create a function to compare 2 arrays that returns the number of values
    in common between the arrays. Use the 2 example arrays and make sure
    your solution uses array_search().

--- php/search_arrays.php
+++ php/search_arrays.php
@@ -1,30 +1,39 @@
 <?php
 
-
-// first names
 $names = ['Tina', 'Dana', 'Mike', "Amy", 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
-// Create a function that returns true or false if an array value is found.
 
-function found_value($value, $array, $strict = TRUE){
-	$result = array_search($value, $array);
-	if ($result == TRUE) {		
-		return TRUE;
-	} else {
-		return FALSE;
-	}
+// Exercise 2
+// function found_value($value, $array, $strict = TRUE){
+// 	$result = array_search($value, $array);
+// 	if ($result == TRUE) {		
+// 		return TRUE;
+// 	} else {
+// 		return FALSE;
+// 	}
+// }
+
+// found_value('Tina', $names);
+// found_value('Bob', $names);
+
+
+// Exercise 3
+
+function common_values($array1, $array2){
+ $number_in_common = 0;
+ foreach ($array1 as $array1) {
+     $result = array_search($array1, $array2);
+
+     if (is_numeric($result)) {
+         $number_in_common++;
+     }
+ }
+     return $number_in_common;
 }
 
-found_value('Tina', $names);
-found_value('Bob', $names);
-
-
-
-
-
-	
+echo common_values($names, $compare) . PHP_EOL;
 
 
 
